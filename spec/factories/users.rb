c890c4b5c59aca014c40743f7b38e595563fcea7
Chris Gannon <cjgannon@gmail.com>
2016-06-02 13:03:48 +0100

    moved user model to pafs_core

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 FactoryGirl.define do
-  factory :user, class: OpenStruct do
+  factory :user, class: PafsCore::User do
     first_name "Norville"
     last_name "Rogers"
     email "shaggy@example.com"
