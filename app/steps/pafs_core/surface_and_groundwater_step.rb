e46eaa059ff2a995ddaa383d84412d23c5bade47
Tony Headford <tony@binarycircus.com>
2016-06-27 07:46:19 +0100

    Surface and groundwater page 1 and 2 (PK-475, PK-564)

---
+++ app/steps/pafs_core/surface_and_groundwater_step.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+module PafsCore
+  class SurfaceAndGroundwaterStep < BasicStep
+    delegate :improve_surface_or_groundwater,
+      :improve_surface_or_groundwater=,
+      :improve_surface_or_groundwater?,
+      to: :project
+
+    validate :a_choice_has_been_made
+
+    def update(params)
+      assign_attributes(step_params(params))
+      if valid? && project.save
+        @step = if improve_surface_or_groundwater?
+                  :surface_and_groundwater_amount
+                else
+                  :habitat_improvement
+                end
+        true
+      else
+        false
+      end
+    end
+
+    def previous_step
+      :approach
+    end
+
+    def step
+      @step ||= :surface_and_groundwater
+    end
+
+    # override BasicStep#completed? to handle earliest_date step
+    def completed?
+      return false if improve_surface_or_groundwater.nil?
+      # if 'No' selected then no sub-step needed
+      return true if !improve_surface_or_groundwater?
+
+      sub_step = PafsCore::SurfaceAndGroundwaterAmountStep.new(project)
+      sub_step.completed?
+    end
+
+  private
+    def step_params(params)
+      ActionController::Parameters.new(params).
+        require(:surface_and_groundwater_step).
+        permit(:improve_surface_or_groundwater)
+    end
+
+    def a_choice_has_been_made
+      errors.add(:improve_surface_or_groundwater,
+                 "^Tell us if the project protects or improves "\
+                 "surface water or groundwater") if improve_surface_or_groundwater.nil?
+    end
+  end
+end
