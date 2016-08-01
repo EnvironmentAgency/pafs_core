9ce87227ded9b728c057c86aa5720498edbc393b
Chris Gannon <cjgannon@gmail.com>
2016-08-01 13:34:16 +0100

    updated email validation

--- app/models/pafs_core/account_request.rb
+++ app/models/pafs_core/account_request.rb
@@ -5,7 +5,11 @@ module PafsCore
     validates :first_name, presence: { message: "^Tell us your first name." }
     validates :last_name, presence: { message: "^Tell us your last name." }
     validates :email, presence: { message: "^Tell us your work email address." }
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "^Provide a valid work email address." }
+    validates :email, format: {
+      with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+      on: :create,
+      message: "^Provide a valid work email address."
+    }
     validates :email, uniqueness: true
     validates :organisation, presence: { message: "^Tell us the organisation you work for." }
     validates :job_title, presence: { message: "^Tell us your job title." }
