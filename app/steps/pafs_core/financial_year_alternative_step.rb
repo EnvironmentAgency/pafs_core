edd2a0614989695c2a2817b2507101d215c86fae
Tony Headford <tony@binarycircus.com>
2016-08-24 14:37:33 +0100

    Added tests for project summary presenter

--- app/steps/pafs_core/financial_year_alternative_step.rb
+++ app/steps/pafs_core/financial_year_alternative_step.rb
@@ -11,24 +11,5 @@ module PafsCore
                                   .require(:financial_year_alternative_step)
                                   .permit(:project_end_financial_year)
     end
-
-    def project_end_financial_year_is_present_and_correct
-      v = project_end_financial_year
-      if v.blank?
-        errors.add(
-          :project_end_financial_year,
-          "^Tell us the financial year when the project will stop spending funds."
-        )
-      elsif v.to_s =~ /\A\d{4}\z/
-        n = v.to_i
-        if n < current_financial_year
-          errors.add(:project_end_financial_year, "^The financial year must be in the future")
-        elsif n > 2100
-          errors.add(:project_end_financial_year, "must be 2100 or earlier")
-        end
-      else
-        errors.add(:project_end_financial_year, "^The financial year must be valid. For example, 2020.")
-      end
-    end
   end
 end
--- app/steps/pafs_core/financial_year_step.rb
+++ app/steps/pafs_core/financial_year_step.rb
@@ -5,36 +5,11 @@ module PafsCore
 
     validate :project_end_financial_year_is_present_and_correct
 
-    def financial_year_options
-      current_financial_year..current_financial_year + 5
-    end
-
   private
     def step_params(params)
-      ActionController::Parameters.new(params).require(:financial_year_step).permit(:project_end_financial_year)
-    end
-
-    def current_financial_year
-      Time.current.uk_financial_year
-    end
-
-    def project_end_financial_year_is_present_and_correct
-      v = project_end_financial_year
-      if v.blank? || v.zero? || v.nil?
-        errors.add(
-          :project_end_financial_year,
-          "^Tell us the financial year when the project will stop spending funds."
-        )
-      elsif v.to_s =~ /\A\d{4}\z/
-        n = v.to_i
-        if n < current_financial_year
-          errors.add(:project_end_financial_year, "^The financial year must be in the future")
-        elsif n > 2100
-          errors.add(:project_end_financial_year, "must be 2100 or earlier")
-        end
-      else
-        errors.add(:project_end_financial_year, "^The financial year must be valid. For example, 2020.")
-      end
+      ActionController::Parameters.new(params).
+        require(:financial_year_step).
+        permit(:project_end_financial_year)
     end
   end
 end
