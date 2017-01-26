6db8bea389bd086a29da7a7eecda6f92966c2b45
Nigel Johnson <nigel.johnson@environment-agency.gov.uk>
2017-01-26 11:59:24 +0000

    PM-140, PM-168, PM-180, PM-199, PM-207, PM-208

--- lib/pafs_core/project_types.rb
+++ lib/pafs_core/project_types.rb
@@ -5,8 +5,8 @@ module PafsCore
   # DEF - changing the current standard or creating a new asset
   # CM  - reinstating the standard of service of an asset
   # PLP - property level protection
-  # BRG - works to bridges that enable FCERM activities
+  # BRG - works to bridges that enable FCERM activities but REMOVED by PM-207 26 Jan 2017
   # STR - strategies
   # ENV - environmental projects (not SSSI or BAP)
-  PROJECT_TYPES = %w[ DEF CM PLP BRG STR ENV_WITH_HOUSEHOLDS ENV_WITHOUT_HOUSEHOLDS ].freeze
+  PROJECT_TYPES = %w[ DEF CM PLP STR ENV_WITH_HOUSEHOLDS ENV_WITHOUT_HOUSEHOLDS ].freeze
 end
