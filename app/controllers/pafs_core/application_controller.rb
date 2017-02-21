9c43799a85e34dd0c7ac769a4449a35aad4ec804
Tony Headford <tony@binarycircus.com>
2017-02-21 14:55:50 +0000

    Graceful fails (PM-116)

--- app/controllers/pafs_core/application_controller.rb
+++ app/controllers/pafs_core/application_controller.rb
@@ -7,6 +7,8 @@ module PafsCore
 
     before_filter :set_cache_headers
 
+    rescue_from ActionController::InvalidAuthenticityToken, with: :handle_invalid_authenticity_token
+
   private
     def raise_not_found
       raise ActionController::RoutingError.new("Not Found")
@@ -16,6 +18,14 @@ module PafsCore
       response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate, private"
       response.headers["Pragma"] = "no-cache"
       response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
+
+    def handle_invalid_authenticity_token(exception)
+      Airbrake.notify(exception) if defined? Airbrake
+      Rails.logger.error "ApplicationController authenticity failed " \
+                         "(browser cookies may have been disabled): #{exception}"
+
+      render "pafs_core/errors/invalid_authenticity_token"
     end
   end
 end
