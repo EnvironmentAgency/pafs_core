49faf490fe29125102c6381f3b5be27a5980366e
Yomi Colledge <baphled@boodah.net>
2018-04-27 10:31:17 +0100

    No need to set FundingCalculatorSummaryStep#uploaded_file in our steps factories

--- spec/factories/steps.rb
+++ spec/factories/steps.rb
@@ -175,7 +175,6 @@ FactoryGirl.define do
 
     factory :funding_calculator_step, class: PafsCore::FundingCalculatorStep do
       funding_calculator_file_name  "pf_calc.xls"
-      uploaded_file                 File.join(Rails.root, '..', 'fixtures', 'calculator.xlsx')
     end
 
     factory :funding_calculator_summary_step, class: PafsCore::FundingCalculatorSummaryStep do
