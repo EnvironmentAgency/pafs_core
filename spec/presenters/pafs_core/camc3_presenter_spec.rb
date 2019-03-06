1583f09bcb56459d0a0d599139a5db77aa56d08a
Yomi Colledge <baphled@boodah.net>
2019-03-06 14:38:35 +0000

    Refactor specs

--- spec/presenters/pafs_core/camc3_presenter_spec.rb
+++ spec/presenters/pafs_core/camc3_presenter_spec.rb
@@ -47,49 +47,61 @@ RSpec.describe PafsCore::Camc3Presenter do
   end
 
   describe '#households_at_reduced_risk' do
-    it 'has the forecast for the households at reduced risks' do
+    before(:each) do
       funding_values.each do |hash|
         project.flood_protection_outcomes.create(
           households_at_reduced_risk: hash[:value],
           financial_year: hash[:year]
         )
       end
+    end
+
+    it 'has the forecast for the households at reduced risks' do
       expect(subject.households_at_reduced_risk).to eql(outcome_measurements)
     end
 
   end
   describe '#moved_from_very_significant_and_significant_to_moderate_or_low' do
-    it 'has the forecast for the households moved from very significant and significant to moderate or low' do
+    before(:each) do
       funding_values.each do |hash|
         project.flood_protection_outcomes.create(
           moved_from_very_significant_and_significant_to_moderate_or_low: hash[:value],
           financial_year: hash[:year]
         )
       end
+    end
+
+    it 'has the forecast for the households moved from very significant and significant to moderate or low' do
       expect(subject.moved_from_very_significant_and_significant_to_moderate_or_low).to eql(outcome_measurements)
     end
   end
 
   describe '#households_protected_from_loss_in_20_percent_most_deprived' do
-    it 'has the forecast for the households protected from loss in 20 percent most deprived' do
+    before(:each) do
       funding_values.each do |hash|
         project.flood_protection_outcomes.create(
           households_protected_from_loss_in_20_percent_most_deprived: hash[:value],
           financial_year: hash[:year]
         )
       end
+    end
+
+    it 'has the forecast for the households protected from loss in 20 percent most deprived' do
       expect(subject.households_protected_from_loss_in_20_percent_most_deprived).to eql(outcome_measurements)
     end
 
   end
   describe '#coastal_households_at_reduced_risk' do
-    it 'has the forecast for the coastal households at reduced risk' do
+    before(:each) do
       funding_values.each do |hash|
         project.coastal_erosion_protection_outcomes.create(
           households_at_reduced_risk: hash[:value],
           financial_year: hash[:year]
         )
       end
+    end
+
+    it 'has the forecast for the coastal) households at reduced risk' do
       expect(subject.coastal_households_at_reduced_risk).to eql(outcome_measurements)
     end
   end
