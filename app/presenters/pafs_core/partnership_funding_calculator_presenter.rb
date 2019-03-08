c599bdcef1b1c94f00fa4c9a8ea3118f5e4ce9f6
Yomi Colledge <baphled@boodah.net>
2019-03-08 14:06:57 +0000

    Revert refactor to keep tests passing

--- app/presenters/pafs_core/partnership_funding_calculator_presenter.rb
+++ app/presenters/pafs_core/partnership_funding_calculator_presenter.rb
@@ -2,27 +2,21 @@ class PafsCore::PartnershipFundingCalculatorPresenter
   include PafsCore::Files
 
   attr_accessor :project
-  attr_accessor :file
 
   attr_accessor :mapper
   attr_accessor :pv_appraisal_approach
 
-  def initialize(project:, file: nil)
-    self.file = file
-    fetch_funding_calculator(project)
-    self.mapper = PafsCore::Mapper::PartnershipFundingCalculator.new(calculator_file: File.open(self.file))
+  def initialize(project:)
+    fetch_funding_calculator_for(project) do |data, filename, content_type|
+      file = Tempfile.new(filename)
+      file.write(data)
+      file.close
+
+      self.mapper = PafsCore::Mapper::PartnershipFundingCalculator.new(calculator_file: File.open(file))
+    end
   end
 
   def attributes
     mapper.data
-  end
-
-  protected
-  def fetch_funding_calculator(project)
-    fetch_funding_calculator_for(project) do |data, filename, content_type|
-      self.file = Tempfile.new(filename)
-      self.file.write(data)
-      self.file.close
-    end
   end
 end
