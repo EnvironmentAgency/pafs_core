ab550a84a99e493bdabbb07163b6463b53c7cd44
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-23 12:35:39 +0100

    [PM-438] Add contributor factory and refactor values factory

--- spec/factories/pafs_core_funding_contributors.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :pafs_core_funding_contributor, class: 'PafsCore::FundingContributor' do
-    name { "MyString" }
-    funding_value_id { 1 }
-    amount { 1 }
-  end
-end
