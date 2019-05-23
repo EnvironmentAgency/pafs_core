0e4c96b9bc4d5ed18fb21370598624b300478636
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-23 12:02:57 +0100

    [PM-440] Refactor funding values factories and specs

--- spec/factories/funding_values.rb
+++ spec/factories/funding_values.rb
@@ -1,22 +1,19 @@
 # frozen_string_literal: true
 FactoryBot.define do
-  factory :funding_values, class: PafsCore::FundingValue do
-    project_id { 1 }
+  factory :funding_value, class: PafsCore::FundingValue do
+    project
     financial_year { 2020 }
     fcerm_gia { 120000 }
+    local_levy { 101010 }
+    public_contributions { 202020 }
+    private_contributions { 303030 }
+    other_ea_contributions { 404040 }
+    growth_funding { 505050 }
+    internal_drainage_boards { 606060 }
+    not_yet_identified { 707070 }
 
-    factory :previous_year do
+    trait :previous_year do
       financial_year { -1 }
-    end
-
-    factory :full_funding_values do
-      local_levy { 101010 }
-      public_contributions { 202020 }
-      private_contributions { 303030 }
-      other_ea_contributions { 404040 }
-      growth_funding { 505050 }
-      internal_drainage_boards { 606060 }
-      not_yet_identified { 707070 }
     end
   end
 end
