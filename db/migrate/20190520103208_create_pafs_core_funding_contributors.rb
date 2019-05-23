13cfac627a89dcfdf59adedb73f9f5930a455f93
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-23 13:57:11 +0100

    [PM-438] Add type to differentiate function types

--- db/migrate/20190520103208_create_pafs_core_funding_contributors.rb
+++ db/migrate/20190520103208_create_pafs_core_funding_contributors.rb
@@ -2,6 +2,7 @@ class CreatePafsCoreFundingContributors < ActiveRecord::Migration
   def change
     create_table :pafs_core_funding_contributors do |t|
       t.string :name
+      t.string :type
       t.integer :funding_value_id
       t.integer :amount
       t.boolean :secured
@@ -9,6 +10,9 @@ class CreatePafsCoreFundingContributors < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
     add_index :pafs_core_funding_contributors, :funding_value_id
+    add_index :pafs_core_funding_contributors, :type
+    add_index :pafs_core_funding_contributors, [:funding_value_id, :type], name: :funding_contributors_on_funding_value_id_and_type
   end
 end
