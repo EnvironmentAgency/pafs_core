e482e028c0f620e5cc0802ed51d02dea73635347
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-23 12:35:39 +0100

    [PM-438] Add contributor factory and refactor values factory

--- spec/factories/funding_values.rb
+++ spec/factories/funding_values.rb
@@ -5,9 +5,6 @@ FactoryBot.define do
     financial_year { 2020 }
     fcerm_gia { 120000 }
     local_levy { 101010 }
-    public_contributions { 202020 }
-    private_contributions { 303030 }
-    other_ea_contributions { 404040 }
     growth_funding { 505050 }
     internal_drainage_boards { 606060 }
     not_yet_identified { 707070 }
