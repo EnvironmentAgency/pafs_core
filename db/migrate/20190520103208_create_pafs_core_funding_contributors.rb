f90f94c89eb0cade16ba72ea47481b15ce27af4d
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-23 15:01:02 +0100

    [PM-438] Move to contributor type and update mapper

--- db/migrate/20190520103208_create_pafs_core_funding_contributors.rb
+++ db/migrate/20190520103208_create_pafs_core_funding_contributors.rb
@@ -2,7 +2,7 @@ class CreatePafsCoreFundingContributors < ActiveRecord::Migration
   def change
     create_table :pafs_core_funding_contributors do |t|
       t.string :name
-      t.string :type
+      t.string :contributor_type
       t.integer :funding_value_id
       t.integer :amount
       t.boolean :secured
@@ -12,7 +12,7 @@ class CreatePafsCoreFundingContributors < ActiveRecord::Migration
     end
 
     add_index :pafs_core_funding_contributors, :funding_value_id
-    add_index :pafs_core_funding_contributors, :type
-    add_index :pafs_core_funding_contributors, [:funding_value_id, :type], name: :funding_contributors_on_funding_value_id_and_type
+    add_index :pafs_core_funding_contributors, :contributor_type
+    add_index :pafs_core_funding_contributors, [:funding_value_id, :contributor_type], name: :funding_contributors_on_funding_value_id_and_type
   end
 end
