563db1211c3df75770865a7df85223d9ce9bfc9a
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-23 13:58:29 +0100

    [PM-438] Fix for funding values spec

--- spec/models/pafs_core/funding_value_spec.rb
+++ spec/models/pafs_core/funding_value_spec.rb
@@ -3,22 +3,20 @@ require "rails_helper"
 
 RSpec.describe PafsCore::FundingValue, type: :model do
   describe "attributes" do
-    subject { FactoryBot.create(:funding_values) }
+    subject { FactoryBot.create(:funding_value) }
 
     it { is_expected.to validate_numericality_of(:fcerm_gia).allow_nil }
     it { is_expected.to validate_numericality_of(:local_levy).allow_nil }
     it { is_expected.to validate_numericality_of(:internal_drainage_boards).allow_nil }
-    it { is_expected.to validate_numericality_of(:public_contributions).allow_nil }
-    it { is_expected.to validate_numericality_of(:private_contributions).allow_nil }
-    it { is_expected.to validate_numericality_of(:other_ea_contributions).allow_nil }
     it { is_expected.to validate_numericality_of(:growth_funding).allow_nil }
     it { is_expected.to validate_numericality_of(:not_yet_identified).allow_nil }
   end
 
   describe ".previous_years" do
+    let(:project) { FactoryBot.create(:project) }
+
     before(:each) do
-      @project = FactoryBot.create(:project)
-      @values = FactoryBot.create(:previous_year, project_id: @project.id)
+      @values = FactoryBot.create(:funding_value, :previous_year, project: project)
     end
     it "returns records with :financial_year set to -1" do
       records = described_class.previous_years
@@ -27,13 +25,13 @@ RSpec.describe PafsCore::FundingValue, type: :model do
   end
 
   describe ".to_financial_year" do
+    let(:project) { FactoryBot.create(:project) }
     before(:each) do
-      @project = FactoryBot.create(:project)
       @values = [
-        FactoryBot.create(:previous_year, project_id: @project.id),
-        FactoryBot.create(:funding_values, project_id: @project.id, financial_year: 2017),
-        FactoryBot.create(:funding_values, project_id: @project.id, financial_year: 2018),
-        FactoryBot.create(:funding_values, project_id: @project.id, financial_year: 2019)
+        FactoryBot.create(:funding_value, :previous_year, project: project),
+        FactoryBot.create(:funding_value, project: project, financial_year: 2017),
+        FactoryBot.create(:funding_value, project: project, financial_year: 2018),
+        FactoryBot.create(:funding_value, project: project, financial_year: 2019)
       ]
     end
     it "returns records upto and including the specified year" do
@@ -44,9 +42,12 @@ RSpec.describe PafsCore::FundingValue, type: :model do
   end
 
   it "calculates the total before saving" do
-    values = FactoryBot.build(:funding_values)
+    values = FactoryBot.build(:funding_value)
     values.fcerm_gia = 2_500_000
     values.local_levy = 1_000_000
-    expect { values.save }.to change { values.total }.to(3_500_000)
+    values.internal_drainage_boards = 100
+    values.growth_funding = 1_000
+    values.not_yet_identified = 2_500
+    expect { values.save }.to change { values.total }.to(3_503_600)
   end
 end
