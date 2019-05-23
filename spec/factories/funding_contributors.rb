56743f33465bd1bc787b910c3aa3d9dfaef46f31
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-23 15:01:02 +0100

    [PM-438] Move to contributor type and update mapper

--- spec/factories/funding_contributors.rb
+++ spec/factories/funding_contributors.rb
@@ -1,9 +1,20 @@
 FactoryBot.define do
   factory :funding_contributor, class: 'PafsCore::FundingContributor' do
     name { "EnviroCo Ltd" }
+    contributor_tyoe { PafsCore::FundingSources::PUBLIC_CONTRIBUTION }
     funding_value
     amount { 1000 }
     secured { true }
     constrained { false }
+
+    trait :public_contributor do
+      contributor_tyoe { PafsCore::FundingSources::PUBLIC_CONTRIBUTION }
+    end
+    trait :private_contributor do
+      contributor_tyoe { PafsCore::FundingSources::PRIVATE_CONTRIBUTION }
+    end
+    trait :ea_contributor do
+      contributor_tyoe { PafsCore::FundingSources::EA_CONTRIBUTION }
+    end
   end
 end
