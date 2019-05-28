5fbb2e85e948687e38d7bd3142b5933ac248df58
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-28 11:31:44 +0100

    [PM-440] Refactor summing of contributor totals

--- app/models/pafs_core/funding_value.rb
+++ app/models/pafs_core/funding_value.rb
@@ -6,7 +6,7 @@ module PafsCore
     has_many :funding_contributors, dependent: :destroy
     has_many :public_contributions, -> { where(contributor_type: FundingSources::PUBLIC_CONTRIBUTIONS) }, class_name: 'PafsCore::FundingContributor'
     has_many :private_contributions, -> { where(contributor_type: FundingSources::PRIVATE_CONTRIBUTIONS) }, class_name: 'PafsCore::FundingContributor'
-    has_many :ea_contributions, -> { where(contributor_type: FundingSources::EA_CONTRIBUTIONS) }, class_name: 'PafsCore::FundingContributor'
+    has_many :other_ea_contributions, -> { where(contributor_type: FundingSources::EA_CONTRIBUTIONS) }, class_name: 'PafsCore::FundingContributor'
 
     validates :fcerm_gia,
               :local_levy,
@@ -25,17 +25,17 @@ module PafsCore
       where(arel_table[:financial_year].lteq(year.to_i)).order(:financial_year)
     end
 
+    FundingSources::FUNDING_SOURCES.each do |source|
+      define_method("#{source}_total") do
+        return public_send(source).to_i unless FundingSources::AGGREGATE_SOURCES.include?(source)
+        (public_send(source) || []).map(&:amount).sum.to_i
+      end
+    end
+
     private
 
     def update_total
-      self.total = FundingSources::FUNDING_SOURCES.reduce(0) do |tot, f|
-        v = FundingSources::AGGREGATE_SOURCES.include?(f) ? (send(f) || []).sum(&:amount) : send(f)
-        if v.present?
-          tot + v.to_i
-        else
-          tot
-        end
-      end
+      self.total = FundingSources::FUNDING_SOURCES.reduce(0) {|tot, f| tot += public_send("#{f}_total") }
     end
   end
 end
