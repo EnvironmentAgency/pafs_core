f5d8966815e82bc2c2c0ad0eb055d4bb10f57748
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-30 15:02:16 +0100

    Spec fixes

--- spec/steps/pafs_core/public_contributors_step_spec.rb
+++ spec/steps/pafs_core/public_contributors_step_spec.rb
@@ -2,42 +2,10 @@
 require "rails_helper"
 
 RSpec.describe PafsCore::PublicContributorsStep, type: :model do
-  subject { FactoryBot.build(:public_contributors_step) }
+  let(:project) { create(:project) }
+  subject { described_class.new(project) }
 
   describe "attributes" do
     it_behaves_like "a project step"
-
-    it "requires :public_contributor_names to be present" do
-      subject.public_contributor_names = nil
-      expect(subject.valid?).to be false
-      expect(subject.errors.messages[:public_contributor_names]).to include "^Tell us the public sector contributors."
-    end
-  end
-
-  describe "#update" do
-    subject { FactoryBot.create(:public_contributors_step) }
-    let(:params) {
-      HashWithIndifferentAccess.new(
-        { public_contributors_step: {
-            public_contributor_names: "Chigley District Council" }
-        }
-      )
-    }
-    let(:error_params) {
-      HashWithIndifferentAccess.new(
-        { public_contributors_step: {
-            public_contributor_names: nil }
-        }
-      )
-    }
-
-    it "saves the state of valid params" do
-      expect(subject.update(params)).to eq true
-      expect(subject.public_contributor_names).to eq "Chigley District Council"
-    end
-
-    it "returns false when validation fails" do
-      expect(subject.update(error_params)).to eq false
-    end
   end
 end
