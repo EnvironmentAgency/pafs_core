4ca1a66950265c223f89a00c0f3cbddbeeffece5
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-30 15:02:16 +0100

    Spec fixes

--- spec/steps/pafs_core/other_ea_contributors_step_spec.rb
+++ spec/steps/pafs_core/other_ea_contributors_step_spec.rb
@@ -2,43 +2,10 @@
 require "rails_helper"
 
 RSpec.describe PafsCore::OtherEaContributorsStep, type: :model do
-  subject { FactoryBot.build(:other_ea_contributors_step) }
+  let(:project) { create(:project) }
+  subject { described_class.new(project) }
 
   describe "attributes" do
     it_behaves_like "a project step"
-
-    it "requires :other_ea_contributor_names to be present" do
-      subject.other_ea_contributor_names = nil
-      expect(subject.valid?).to be false
-      expect(subject.errors.messages[:other_ea_contributor_names]).
-        to include "^Tell us about the expected contributions from other Environment Agency functions or sources."
-    end
-  end
-
-  describe "#update" do
-    subject { FactoryBot.create(:other_ea_contributors_step) }
-    let(:params) {
-      HashWithIndifferentAccess.new(
-        { other_ea_contributors_step: {
-            other_ea_contributor_names: "Chigley Water" }
-        }
-      )
-    }
-    let(:error_params) {
-      HashWithIndifferentAccess.new(
-        { other_ea_contributors_step: {
-            other_ea_contributor_names: nil }
-        }
-      )
-    }
-
-    it "saves the state of valid params" do
-      expect(subject.update(params)).to eq true
-      expect(subject.other_ea_contributor_names).to eq "Chigley Water"
-    end
-
-    it "returns false when validation fails" do
-      expect(subject.update(error_params)).to eq false
-    end
   end
 end
