8ca5955e91aec1f71f1a04612a6cb62d0d1e3299
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-06-07 13:40:19 +0100

    [PM-440] Add JS to add and remove contributors

---
+++ app/assets/javascripts/pafs_core/funding_contributors.js
@@ -0,0 +1,40 @@
+function MultipleElementForm(element_selector) {
+  var self = this;
+  var selector = element_selector;
+  var element_template = $(selector).find('.form-group').first().clone();
+  var delete_button = $("<a href='#' class='column-one-half delete_button'>Remove contributor</a>");
+
+  self.addElement = function(evt) {
+    var new_element = element_template.clone();
+    var new_delete_button = delete_button.clone();
+
+    evt.preventDefault();
+
+    new_element.find('input').val('');
+    new_element.append(new_delete_button);
+    new_element.insertBefore($(selector).find('a.add_element').parent());
+  }
+
+  self.removeElement = function(evt) {
+    evt.preventDefault();
+    $(evt.target).parent().remove()
+  }
+
+  self.initialize = function () {
+    var add_button = $("<p><a href='#' class='add_element'>Add another contributor</a></p>");
+
+    $(selector).append(add_button);
+    add_button.on('click', this.addElement.bind(this));
+
+    $(selector).find('>.form-group div').each(function(index, elem) {
+      $(elem).append(delete_button.clone()) 
+    });
+
+    $(selector).delegate('.delete_button', 'click', this.removeElement.bind(this));
+  }
+}
+
+$(function() {
+  var element_form = new MultipleElementForm("form .contributors");
+  element_form.initialize();
+});
