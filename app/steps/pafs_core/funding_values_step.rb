e1761f073f3ba61096c318efe5f8b121541706dc
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-06-24 14:16:37 +0100

    [PM-499] Remove validation on save funding values step

--- app/steps/pafs_core/funding_values_step.rb
+++ app/steps/pafs_core/funding_values_step.rb
@@ -9,8 +9,6 @@ module PafsCore
     # This sorts the sources with the aggregated sources at the end of the array
     SORTED_SOURCES = (FUNDING_SOURCES - AGGREGATE_SOURCES) + AGGREGATE_SOURCES
 
-    validate :at_least_one_value_per_column_entered
-
     def update(params)
       clean_unselected_funding_sources
       super
@@ -35,28 +33,6 @@ module PafsCore
           :total
         ].concat(FUNDING_SOURCES - AGGREGATE_SOURCES)
       )
-    end
-
-    def at_least_one_value_per_column_entered
-      if selected_funding_sources.empty?
-        # this is only for the nav so we don't get a tick when no funding sources
-        # have been selected yet
-        errors.add(:base, "You must select at least one funding source first")
-      else
-        (selected_funding_sources - AGGREGATE_SOURCES).each do |fs|
-          found = false
-          funding_values.each do |fv|
-            val = fv.send(fs)
-            if val.present?
-              found = true
-              errors.add(:base, "Values must be greater than or equal to zero") if val.to_i < 0
-            end
-          end
-          errors.add(
-            :base, "In the applicable year(s), tell us the estimated spend for #{funding_source_label(fs)}"
-          ) unless found
-        end
-      end
     end
   end
 end
