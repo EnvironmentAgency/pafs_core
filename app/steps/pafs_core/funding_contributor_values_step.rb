76a1b6e340bad587ed5ce563c53025cfa5632883
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-07-02 09:36:24 +0100

    [PM-511] Ensure each contributor has at least one value

--- app/steps/pafs_core/funding_contributor_values_step.rb
+++ app/steps/pafs_core/funding_contributor_values_step.rb
@@ -10,6 +10,8 @@ module PafsCore
              to: :project
 
     def update(params)
+      return false unless at_least_one_value(params)
+
       PafsCore::FundingContributor.transaction do
         step_params(params)["funding_contributors"].each do |id, attrs|
           funding_contributors.find(id).update!(attrs)
@@ -28,13 +30,28 @@ module PafsCore
 
     private
 
+    def at_least_one_value(params)
+      attributes = step_params(params)["funding_contributors"].values
+
+      contributor_names = attributes.map { |attrs| attrs['name'] }.uniq
+      contributors_with_at_least_one_value = attributes.map do |attrs| 
+        attrs['amount'].to_i > 0 ? attrs['name'] : nil
+      end.compact.uniq
+
+      return true if (contributor_names - contributors_with_at_least_one_value).empty?
+
+      errors.add(:base, "Please ensure you enter at lease one value for every contributor")
+      false
+    end
+
     def step_params(params)
       ActionController::Parameters.new(params).require(param_key).permit(
         funding_contributors: [
           :id,
           :amount,
           :secured,
-          :constrained
+          :constrained,
+          :name
         ]
       )
     end
