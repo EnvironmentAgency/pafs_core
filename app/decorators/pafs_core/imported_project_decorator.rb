fc8f21ffd5927b9014c2c7d30fa30752f33b7ef4
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-07-02 09:59:05 +0100

    [PM-512] Add validation to funding values table

--- app/decorators/pafs_core/imported_project_decorator.rb
+++ app/decorators/pafs_core/imported_project_decorator.rb
@@ -102,15 +102,15 @@ module PafsCore
     end
 
     def public_contributors=(value)
-      project.public_contributions = value
+      project.public_contributions = !value.strip.blank?
     end
 
     def private_contributors=(value)
-      project.private_contributions = value
+      project.private_contributions = !value.strip.blank?
     end
 
     def other_ea_contributors=(value)
-      project.other_ea_contributions = value
+      project.other_ea_contributions = !value.strip.blank?
     end
 
     def earliest_start_date=(value)
