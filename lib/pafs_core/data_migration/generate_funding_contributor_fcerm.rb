a8ea4e35229fca722fd33b1d7bf9eb1e5cebd160
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-09-30 14:33:09 +0100

    [PM-535] Add task to generate FCERM for only relevant proposals

---
+++ lib/pafs_core/data_migration/generate_funding_contributor_fcerm.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module PafsCore
+  module DataMigration
+    class GenerateFundingContributorFcerm
+      include PafsCore::Files, PafsCore::FileStorage
+
+      def self.perform(user)
+        new(user).perform
+      end
+
+      attr_reader :user
+
+      def initialize(user)
+        @user = user
+      end
+
+      def perform
+        PafsCore::AreaDownload.transaction do
+          download_info.save!
+          generate_multi_fcerm1(projects, download_info.fcerm1_filename)
+          download_info.documentation_state.complete!
+          download_info.save!
+        end
+      end
+
+      def area
+        user.primary_area
+      end
+
+      def download_info
+        @download_info ||= (area.area_download || area.create_area_download).tap do |dl|
+          dl.number_of_proposals = projects.count
+          dl.fcerm1_filename = apt_fcerm1_storage_filename(area)
+          dl.user = user
+          dl.requested_on = Time.zone.now
+          dl.documentation_state.generate!
+        end
+      end
+
+      def projects
+        @projects ||= PafsCore::Project.where(
+          'private_contributions = true OR public_contributions = true or other_ea_contributions = true'
+        )
+      end
+    end
+  end
+end
--- lib/tasks/pafs_core_tasks.rake
+++ lib/tasks/pafs_core_tasks.rake
@@ -5,4 +5,9 @@ namespace :pafs do
   task remove_duplicate_states: :environment do
     PafsCore::DataMigration::RemoveDuplicateStates.perform_all
   end
+
+  task generate_funding_contributor_fcerm: :environment do
+    user = PafsCore::User.find(ENV.fetch('USER_ID'))
+    PafsCore::DataMigration::GenerateFundingContributorFcerm.perform(user)
+  end
 end
