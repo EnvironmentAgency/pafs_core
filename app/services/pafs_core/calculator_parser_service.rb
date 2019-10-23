7e572e5d2861e0537242f2717710a7a820a631cb
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-10-23 17:18:35 +0100

    [PM-551] Add v9 PFC

--- app/services/pafs_core/calculator_parser_service.rb
+++ app/services/pafs_core/calculator_parser_service.rb
@@ -21,15 +21,17 @@ module PafsCore
     def extract_data(calculator, project)
       sheet = calculator.sheet("PF Calculator")
       data = {}
-      data[:strategic_approach] = binary_value(sheet.cell("J", 25))
-      data[:raw_partnership_funding_score] = sheet.cell("H", 15)
-      data[:adjusted_partnership_funding_score] = sheet.cell("H", 19)
-      data[:pv_whole_life_costs] = sheet.cell("H", 37)
-      data[:pv_whole_life_benefits] = sheet.cell("H", 29)
-      data[:duration_of_benefits] = sheet.cell("H", 27)
-      data[:hectares_of_net_water_dependent_habitat_created] = sheet.cell("C", 81)
-      data[:hectares_of_net_water_intertidal_habitat_created] = sheet.cell("C", 82)
-      data[:kilometres_of_protected_river_improved] = sheet.cell("C", 83)
+      data[:strategic_approach] = binary_value(sheet.cell("E", 17))
+      data[:raw_partnership_funding_score] = sheet.cell("E", 19)
+      data[:adjusted_partnership_funding_score] = sheet.cell("K", 19)
+      data[:pv_whole_life_costs] = sheet.cell("E", 33)
+      data[:pv_whole_life_benefits] = sheet.cell("E", 37)
+      data[:duration_of_benefits] = sheet.cell("E", 38)
+
+      # These no longer exist in the PFC. Should be zeroed.
+      # data[:hectares_of_net_water_dependent_habitat_created] = sheet.cell("C", 81)
+      # data[:hectares_of_net_water_intertidal_habitat_created] = sheet.cell("C", 82)
+      # data[:kilometres_of_protected_river_improved] = sheet.cell("C", 83)
 
       #data
       project.update(data)
