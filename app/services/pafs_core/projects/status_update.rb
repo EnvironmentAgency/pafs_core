e7eed0ed4c4e497815b94d9be181d4b38d6812c2
Matt Hall <49390835+MattHallEA@users.noreply.github.com>
2020-01-07 10:56:46 +0000

    [PM-546] Allow imports to specify the project status (#277)
    * [PM-546] Allow imports to specify the project status
    
    * FCERM import would previously not correctly coerce the project status
    * This lead to the status always being set to draft
    * This fix correctly coerces the values from the status filed and
    assigns the correct state.
    
    * [PM-546] Fix failing spec if the status is nil

---
+++ app/services/pafs_core/projects/status_update.rb
@@ -0,0 +1,33 @@
+module PafsCore
+  module Projects
+    class StatusUpdate
+      attr_reader :project, :status
+
+      STATUS_MAP = {
+        'Draft': :draft,
+        'Review': :completed,
+        'Submitted': :submitted,
+        'Archived': :archived
+      }
+
+      def initialize(project, status)
+        @project = project
+        @status = status&.to_sym
+      end
+
+      def coerced_status
+        return status if STATUS_MAP.values.include?(status)
+
+        STATUS_MAP[status] || :draft
+      end
+
+      def state
+        project.state || project.create_state
+      end
+
+      def perform
+        state.update_attribute(:state, coerced_status)
+      end
+    end
+  end
+end
