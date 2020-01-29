919cea469f4860c20113d560720ab972581caf16
Matt Hall <49390835+MattHallEA@users.noreply.github.com>
2020-01-29 12:12:00 +0000

    [PM-556] Add migration for controlled lists (#283)

---
+++ lib/pafs_core/data_migration/update_areas.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module PafsCore
+  module DataMigration
+    class UpdateAreas
+      attr_reader :csv_path
+
+      def self.perform(csv_path)
+        new(csv_path).tap(&:perform)
+      end
+
+      def initialize(csv_path)
+        @csv_path = csv_path
+      end
+
+      def perform
+        not_found = []
+
+        CSV.foreach(csv_path, headers: true) do |row|
+          begin
+            if row['PAFS_LRMA_NAME'] == row['POL_LRMA_NAME']
+              next
+            end
+
+            area = PafsCore::Area.find_by(name: row['PAFS_LRMA_NAME'])
+            destination_area = PafsCore::Area.where.not(
+              id: (area ? area.id : nil)
+            ).find_by(name: row['POL_LRMA_NAME'])
+
+            if destination_area && area.nil?
+              next
+            end
+
+            if area.nil?
+              not_found << row
+              next
+            end
+
+            if destination_area
+              puts "MERGE  [#{area.id} #{destination_area.id}] #{area.name} => #{row['POL_LRMA_NAME']}"
+              PafsCore::AreaProject.where(area_id: area.id).update_all(area_id: destination_area.id)
+              PafsCore::UserArea.where(area_id: area.id).update_all(area_id: destination_area.id)
+              area.destroy
+            else
+              puts "RENAME [#{area.id}] #{area.name} => #{row['POL_LRMA_NAME']}"
+              area.update_column(:name, row['POL_LRMA_NAME'])
+            end
+          end
+        end
+
+        if not_found.any?
+          puts "Unable to update #{not_found.size} rows"
+          puts
+          puts not_found.to_yaml
+        end
+      end
+    end
+  end
+end
--- lib/tasks/pafs_core_tasks.rake
+++ lib/tasks/pafs_core_tasks.rake
@@ -5,4 +5,16 @@ namespace :pafs do
   task remove_duplicate_states: :environment do
     PafsCore::DataMigration::RemoveDuplicateStates.perform_all
   end
+
+  task update_areas: :environment do
+    PafsCore::DataMigration::UpdateAreas.perform(
+      File.join(Rails.root, 'lib', 'fixtures', 'area_migration.csv')
+    )
+  end
+
+  task update_project_areas: :environment do
+    PafsCore::DataMigration::UpdateProjects.perform(
+      File.join(Rails.root, 'lib', 'fixtures', 'project_area_migration.csv')
+    )
+  end
 end
