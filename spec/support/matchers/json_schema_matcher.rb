0b6383cfbb24fc15ddaed0e1297810a7c5e3642d
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-02-10 17:05:24 +0000

    [PM-567] Add json schema
    * Schema to verify camc3 payload json
    * Integrated into specs

---
+++ spec/support/matchers/json_schema_matcher.rb
@@ -0,0 +1,7 @@
+RSpec::Matchers.define :match_json_schema do |schema|
+  match do |json|
+    schema_directory = File.join('doc', 'json_schemas')
+    schema_path = File.join(schema_directory, "#{schema}.json")
+    JSON::Validator.validate!(schema_path, json, strict: true)
+  end
+end
