09746e9a831e8511f8bfe36cf37382e517b55caa
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-06 10:38:14 +0000

    Update PFC mapping

--- lib/pafs_core/mapper/funding_calculator_maps/v9.rb
+++ lib/pafs_core/mapper/funding_calculator_maps/v9.rb
@@ -4,8 +4,11 @@ module PafsCore
   module Mapper
     module FundingCalculatorMaps
       class V9 < Base
+        VERSION_NAME = '2020'
+
         def extract_data
           {
+            partnership_funding_calculator_version:                     VERSION_NAME,
             date_of_pf_calculator:                                      sheet.cell('D', 9),
             lead_rma:                                                   sheet.cell('D', 10),
             project_stage:                                              sheet.cell('M', 7),
@@ -36,167 +39,167 @@ module PafsCore
             pv_funding_from_other_ea_functions_sources_contributor_or_fund:                          sheet.cell('Q', 31),
 
             pv_whole_life_benefits_appraisal_period:       sheet.cell('E', 37),
-            pv_whole_life_benefits_duration_of_benefits:   sheet.cell('E', 39),
-            pv_whole_life_benefits_people_related_impacts: sheet.cell('E', 42),
+            pv_whole_life_benefits_duration_of_benefits:   sheet.cell('E', 38),
+            pv_whole_life_benefits_people_related_impacts: sheet.cell('E', 41),
 
-            economic_sumary_sheet_completed:               sheet.cell('K', 37),
+            economic_summary_sheet_completed:              sheet.cell('K', 37),
             economic_data_included_in_business_case:       sheet.cell('K', 38),
-            wider_benefits_data_included_in_business_case: sheet.cell('K', 39),
+            # wider_benefits_data_included_in_business_case: sheet.cell('K', 39),
 
             qualifying_benefits_outcome_measures: {
               om2_present_day: {
                 before: {
                   most_deprived_20: {
+                    moderate_risk:          sheet.cell('G', 46),
+                    intermediate_risk:      sheet.cell('H', 46),
+                    significant_risk:       sheet.cell('I', 46),
+                    very_significant_risk:  sheet.cell('J', 46)
+                  },
+                  most_deprived_21_40: {
                     moderate_risk:          sheet.cell('G', 47),
                     intermediate_risk:      sheet.cell('H', 47),
                     significant_risk:       sheet.cell('I', 47),
                     very_significant_risk:  sheet.cell('J', 47)
                   },
-                  most_deprived_21_40: {
+                  least_deprived_60: {
                     moderate_risk:          sheet.cell('G', 48),
                     intermediate_risk:      sheet.cell('H', 48),
                     significant_risk:       sheet.cell('I', 48),
                     very_significant_risk:  sheet.cell('J', 48)
-                  },
-                  least_deprived_60: {
-                    moderate_risk:          sheet.cell('G', 49),
-                    intermediate_risk:      sheet.cell('H', 49),
-                    significant_risk:       sheet.cell('I', 49),
-                    very_significant_risk:  sheet.cell('J', 49)
                   }
                 },
                 after: {
                   most_deprived_20: {
+                    low_risk:               sheet.cell('F', 51),
+                    moderate_risk:          sheet.cell('G', 51),
+                    intermediate_risk:      sheet.cell('H', 51),
+                    significant_risk:       sheet.cell('I', 51),
+                    very_significant_risk:  sheet.cell('J', 51)
+                  },
+                  most_deprived_21_40: {
                     low_risk:               sheet.cell('F', 52),
                     moderate_risk:          sheet.cell('G', 52),
                     intermediate_risk:      sheet.cell('H', 52),
                     significant_risk:       sheet.cell('I', 52),
                     very_significant_risk:  sheet.cell('J', 52)
                   },
-                  most_deprived_21_40: {
+                  least_deprived_60: {
                     low_risk:               sheet.cell('F', 53),
                     moderate_risk:          sheet.cell('G', 53),
                     intermediate_risk:      sheet.cell('H', 53),
                     significant_risk:       sheet.cell('I', 53),
                     very_significant_risk:  sheet.cell('J', 53)
-                  },
-                  least_deprived_60: {
-                    low_risk:               sheet.cell('F', 54),
-                    moderate_risk:          sheet.cell('G', 54),
-                    intermediate_risk:      sheet.cell('H', 54),
-                    significant_risk:       sheet.cell('I', 54),
-                    very_significant_risk:  sheet.cell('J', 54)
                   }
                 }
               },
               om2_in_2040: {
                 before: {
                   most_deprived_20: {
+                    moderate_risk:          sheet.cell('G', 61),
+                    intermediate_risk:      sheet.cell('H', 61),
+                    significant_risk:       sheet.cell('I', 61),
+                    very_significant_risk:  sheet.cell('J', 61)
+                  },
+                  most_deprived_21_40: {
                     moderate_risk:          sheet.cell('G', 62),
                     intermediate_risk:      sheet.cell('H', 62),
                     significant_risk:       sheet.cell('I', 62),
                     very_significant_risk:  sheet.cell('J', 62)
                   },
-                  most_deprived_21_40: {
+                  least_deprived_60: {
                     moderate_risk:          sheet.cell('G', 63),
                     intermediate_risk:      sheet.cell('H', 63),
                     significant_risk:       sheet.cell('I', 63),
                     very_significant_risk:  sheet.cell('J', 63)
-                  },
-                  least_deprived_60: {
-                    moderate_risk:          sheet.cell('G', 64),
-                    intermediate_risk:      sheet.cell('H', 64),
-                    significant_risk:       sheet.cell('I', 64),
-                    very_significant_risk:  sheet.cell('J', 64)
                   }
                 },
                 after: {
                   most_deprived_20: {
+                    low_risk:               sheet.cell('F', 66),
+                    moderate_risk:          sheet.cell('G', 66),
+                    intermediate_risk:      sheet.cell('H', 66),
+                    significant_risk:       sheet.cell('I', 66),
+                    very_significant_risk:  sheet.cell('J', 66)
+                  },
+                  most_deprived_21_40: {
                     low_risk:               sheet.cell('F', 67),
                     moderate_risk:          sheet.cell('G', 67),
                     intermediate_risk:      sheet.cell('H', 67),
                     significant_risk:       sheet.cell('I', 67),
                     very_significant_risk:  sheet.cell('J', 67)
                   },
-                  most_deprived_21_40: {
+                  least_deprived_60: {
                     low_risk:               sheet.cell('F', 68),
                     moderate_risk:          sheet.cell('G', 68),
                     intermediate_risk:      sheet.cell('H', 68),
                     significant_risk:       sheet.cell('I', 68),
                     very_significant_risk:  sheet.cell('J', 68)
-                  },
-                  least_deprived_60: {
-                    low_risk:               sheet.cell('F', 69),
-                    moderate_risk:          sheet.cell('G', 69),
-                    intermediate_risk:      sheet.cell('H', 69),
-                    significant_risk:       sheet.cell('I', 69),
-                    very_significant_risk:  sheet.cell('J', 69)
                   }
                 }
               },
               om3: {
                 before_construction: {
                   most_deprived_20: {
+                    long_term_loss:         sheet.cell('F', 74),
+                    medium_term_loss:       sheet.cell('G', 74),
+                  },
+                  most_deprived_21_40: {
                     long_term_loss:         sheet.cell('F', 75),
                     medium_term_loss:       sheet.cell('G', 75),
                   },
-                  most_deprived_21_40: {
+                  least_deprived_60: {
                     long_term_loss:         sheet.cell('F', 76),
                     medium_term_loss:       sheet.cell('G', 76),
-                  },
-                  least_deprived_60: {
-                    long_term_loss:         sheet.cell('F', 77),
-                    medium_term_loss:       sheet.cell('G', 77),
                   },
                 }
               },
               om4: {
                 net_change_hectares: {
                   intertidal_habitat: {
+                    low:    sheet.cell('D', 83),
+                    medium: sheet.cell('E', 83),
+                    high:   sheet.cell('F', 83),
+                  },
+                  woodland: {
                     low:    sheet.cell('D', 84),
                     medium: sheet.cell('E', 84),
                     high:   sheet.cell('F', 84),
                   },
-                  woodland: {
+                  wet_woodland: {
                     low:    sheet.cell('D', 85),
                     medium: sheet.cell('E', 85),
                     high:   sheet.cell('F', 85),
                   },
-                  wet_woodland: {
+                  wetlands_wet_grassland: {
                     low:    sheet.cell('D', 86),
                     medium: sheet.cell('E', 86),
                     high:   sheet.cell('F', 86),
                   },
-                  wetlands_wet_grassland: {
+                  grassland: {
                     low:    sheet.cell('D', 87),
                     medium: sheet.cell('E', 87),
                     high:   sheet.cell('F', 87),
                   },
-                  grassland: {
+                  moor_heath: {
                     low:    sheet.cell('D', 88),
                     medium: sheet.cell('E', 88),
                     high:   sheet.cell('F', 88),
                   },
-                  moor_heath: {
+                  ponds_lakes: {
                     low:    sheet.cell('D', 89),
                     medium: sheet.cell('E', 89),
                     high:   sheet.cell('F', 89),
                   },
-                  ponds_lakes: {
+                  arable_land: {
                     low:    sheet.cell('D', 90),
                     medium: sheet.cell('E', 90),
                     high:   sheet.cell('F', 90),
-                  },
-                  arable_land: {
-                    low:    sheet.cell('D', 91),
-                    medium: sheet.cell('E', 91),
-                    high:   sheet.cell('F', 91),
                   }
                 },
                 length_of_habitat_enhanced: {
-                  comprehensive_restoration_of_physical_modifications: sheet.cell('P', 85),
-                  partial_restoration_of_physical_modifications:       sheet.cell('P', 87),
-                  improvements_to_fish_passage:                        sheet.cell('P', 89)
+                  comprehensive_restoration_of_physical_modifications: sheet.cell('P', 84),
+                  partial_restoration_of_physical_modifications:       sheet.cell('P', 85),
+                  single_major_physical_improvement:                   sheet.cell('P', 86)
                 }
               }
             }
