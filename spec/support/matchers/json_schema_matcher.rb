473ebbf3bfc4a82c3268621259b9f3ebff5ba284
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-13 16:30:50 +0000

    [PM-551] Fix some typos in JSON spec

--- spec/support/matchers/json_schema_matcher.rb
+++ spec/support/matchers/json_schema_matcher.rb
@@ -12,6 +12,6 @@ RSpec::Matchers.define :match_json_schema do |schema|
 
   failure_message do |json|
     json = JSON.parse(json)
-    schemer.validate(json).to_a
+    schemer.validate(json).map {|error| error["details"]}.compact.join("\n")
   end
 end
