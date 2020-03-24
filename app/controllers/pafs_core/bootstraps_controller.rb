f7f4e818fc8ee7d883bac39820c6786a544b629a
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-24 14:12:00 +0000

    [PM-587] Use modules in controllers

--- app/controllers/pafs_core/bootstraps_controller.rb
+++ app/controllers/pafs_core/bootstraps_controller.rb
@@ -1,60 +1,62 @@
 # frozen_string_literal: true
 
-class PafsCore::BootstrapsController < PafsCore::ApplicationController
-  # NOTE: this should be added via a decorator in consuming qpp if needed
-  # before_action :authenticate_user!
+module PafsCore
+  class BootstrapsController < ApplicationController
+    # NOTE: this should be added via a decorator in consuming qpp if needed
+    # before_action :authenticate_user!
 
-  def new
-    # create a temporary bootstrap project and start the steps
-    # for the project name
-    # for the project type
-    # for the financial year the project stops spending funds
-    @project = navigator.create_bootstrap
-    redirect_to bootstrap_step_path(id: @project.to_param, step: @project.step)
-  end
-
-  # GET
-  def step
-    # edit step
-    @project = navigator.find_project_step(params[:id], params[:step])
-    # we want to go to the page in the process requested in the
-    # params[:step] part of the URL and display the appropriate form
-    if @project.disabled?
-      raise_not_found
-    else
-      # give the step the opportunity to do any tasks prior to being viewed
-      @project.before_view(params)
-      # render the step
-      render @project.view_path
+    def new
+      # create a temporary bootstrap project and start the steps
+      # for the project name
+      # for the project type
+      # for the financial year the project stops spending funds
+      @project = navigator.create_bootstrap
+      redirect_to bootstrap_step_path(id: @project.to_param, step: @project.step)
     end
-  end
 
-  # PATCH
-  def save
-    # submit data for the current step and continue or exit
-    # if request is exit redirect to summary or dashboard?
-    # else go to next step
-    @project = navigator.find_project_step(params[:id], params[:step])
-    # each step is responsible for managing their params safely
-    if @project.update(params)
-      next_step = navigator.next_step(@project.step, @project)
-      if next_step == navigator.last_step
-        # we're at the end of the bootstrap
-        # create a project and copy the details over
-        @project = navigator.bootstrap_to_project(@project.to_param)
-        redirect_to project_path(id: @project.to_param)
+    # GET
+    def step
+      # edit step
+      @project = navigator.find_project_step(params[:id], params[:step])
+      # we want to go to the page in the process requested in the
+      # params[:step] part of the URL and display the appropriate form
+      if @project.disabled?
+        raise_not_found
       else
-        redirect_to bootstrap_step_path(id: @project.to_param, step: next_step)
+        # give the step the opportunity to do any tasks prior to being viewed
+        @project.before_view(params)
+        # render the step
+        render @project.view_path
       end
-    else
-      # NOTE: not calling @project.before_view, but we could if we need to
-      render @project.view_path
     end
-  end
 
-  private
+    # PATCH
+    def save
+      # submit data for the current step and continue or exit
+      # if request is exit redirect to summary or dashboard?
+      # else go to next step
+      @project = navigator.find_project_step(params[:id], params[:step])
+      # each step is responsible for managing their params safely
+      if @project.update(params)
+        next_step = navigator.next_step(@project.step, @project)
+        if next_step == navigator.last_step
+          # we're at the end of the bootstrap
+          # create a project and copy the details over
+          @project = navigator.bootstrap_to_project(@project.to_param)
+          redirect_to project_path(id: @project.to_param)
+        else
+          redirect_to bootstrap_step_path(id: @project.to_param, step: next_step)
+        end
+      else
+        # NOTE: not calling @project.before_view, but we could if we need to
+        render @project.view_path
+      end
+    end
 
-  def navigator
-    @navigator ||= PafsCore::BootstrapNavigator.new current_resource
+    private
+
+    def navigator
+      @navigator ||= BootstrapNavigator.new current_resource
+    end
   end
 end
--- app/controllers/pafs_core/downloads_controller.rb
+++ app/controllers/pafs_core/downloads_controller.rb
@@ -1,73 +1,75 @@
 # Play nice with Ruby 3 (and rubocop)
 # frozen_string_literal: true
 
-class PafsCore::DownloadsController < PafsCore::ApplicationController
-  include PafsCore::Files
+module PafsCore
+  class DownloadsController < ApplicationController
+    include PafsCore::Files
 
-  def index
-    @project = PafsCore::ProjectSummaryPresenter.new navigator.find(params[:id])
-  end
+    def index
+      @project = PafsCore::ProjectSummaryPresenter.new navigator.find(params[:id])
+    end
 
-  def proposal
-    @project = PafsCore::ProjectSummaryPresenter.new navigator.find(params[:id])
+    def proposal
+      @project = PafsCore::ProjectSummaryPresenter.new navigator.find(params[:id])
 
-    respond_to do |format|
-      format.csv do
-        # @csv = PafsCore::SpreadsheetBuilderService.new.generate_csv([@project])
-        @csv = generate_fcerm1(@project, :csv)
-        send_data @csv, type: "text/csv", filename: fcerm1_filename(@project.reference_number, :csv)
+      respond_to do |format|
+        format.csv do
+          # @csv = PafsCore::SpreadsheetBuilderService.new.generate_csv([@project])
+          @csv = generate_fcerm1(@project, :csv)
+          send_data @csv, type: "text/csv", filename: fcerm1_filename(@project.reference_number, :csv)
+        end
+
+        format.xlsx do
+          xlsx = generate_fcerm1(@project, :xlsx)
+          send_data xlsx.stream.read,
+                    filename: fcerm1_filename(@project.reference_number, :xlsx)
+        end
       end
+    end
 
-      format.xlsx do
-        xlsx = generate_fcerm1(@project, :xlsx)
-        send_data xlsx.stream.read,
-                  filename: fcerm1_filename(@project.reference_number, :xlsx)
+    def benefit_area
+      @project = navigator.find(params[:id])
+      fetch_benefit_area_file_for(@project) do |data, filename, content_type|
+        send_data(data, filename: filename, type: content_type)
       end
     end
-  end
 
-  def benefit_area
-    @project = navigator.find(params[:id])
-    fetch_benefit_area_file_for(@project) do |data, filename, content_type|
-      send_data(data, filename: filename, type: content_type)
+    # GET
+    def delete_benefit_area
+      @project = navigator.find(params[:id])
+      delete_benefit_area_file_for(@project)
+      redirect_to project_step_path(id: @project.to_param, step: :benefit_area_file)
     end
-  end
 
-  # GET
-  def delete_benefit_area
-    @project = navigator.find(params[:id])
-    delete_benefit_area_file_for(@project)
-    redirect_to project_step_path(id: @project.to_param, step: :benefit_area_file)
-  end
-
-  def funding_calculator
-    @project = navigator.find(params[:id])
-    fetch_funding_calculator_for(@project) do |data, filename, content_type|
-      send_data(data,
-                filename: filename,
-                type: content_type)
+    def funding_calculator
+      @project = navigator.find(params[:id])
+      fetch_funding_calculator_for(@project) do |data, filename, content_type|
+        send_data(data,
+                  filename: filename,
+                  type: content_type)
+      end
     end
-  end
 
-  # GET
-  def delete_funding_calculator
-    @project = navigator.find(params[:id])
-    delete_funding_calculator_for(@project)
-    redirect_to project_step_path(id: @project.to_param, step: :funding_calculator)
-  end
-
-  def moderation
-    @project = navigator.find(params[:id])
-    generate_moderation_for(@project) do |data, filename, content_type|
-      send_data(data,
-                filename: filename,
-                type: content_type)
+    # GET
+    def delete_funding_calculator
+      @project = navigator.find(params[:id])
+      delete_funding_calculator_for(@project)
+      redirect_to project_step_path(id: @project.to_param, step: :funding_calculator)
     end
-  end
 
-  private
+    def moderation
+      @project = navigator.find(params[:id])
+      generate_moderation_for(@project) do |data, filename, content_type|
+        send_data(data,
+                  filename: filename,
+                  type: content_type)
+      end
+    end
 
-  def navigator
-    @navigator ||= PafsCore::ProjectNavigator.new current_resource
+    private
+
+    def navigator
+      @navigator ||= PafsCore::ProjectNavigator.new current_resource
+    end
   end
 end
