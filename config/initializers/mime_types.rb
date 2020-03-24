dc4a657717f242ea43e4ce8ee3db04aa1bea7d05
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-24 10:45:51 +0000

    Setup mime types correctly

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 
-unless defined? Mime::XLSX
-  Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
-end
+Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
