6dee90edd6aa78d16079abe1f6eefab09e5a3f62
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-24 10:09:32 +0000

    [PM-587] Convert specs to RSpec 3.9.1 syntax with Transpec
    This conversion is done by Transpec 3.4.0 with the following command:
        transpec
    
    * 1 addition
          of: RSpec.configure { |c| c.infer_spec_type_from_file_location! }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,15 @@ RSpec.configure do |config|
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
   # Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
+
+  # rspec-rails 3 will no longer automatically infer an example group's spec type
+  # from the file location. You can explicitly opt-in to the feature using this
+  # config option.
+  # To explicitly tag specs without using automatic inference, set the `:type`
+  # metadata manually:
+  #
+  #     describe ThingsController, :type => :controller do
+  #       # Equivalent to being in spec/controllers
+  #     end
+  config.infer_spec_type_from_file_location!
 end
