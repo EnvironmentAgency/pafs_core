fd28f6a6d9c57f58d08a3e04d3aad088c65cd327
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-25 18:59:39 +0000

    [PM-587] Fix migration inheritence

--- db/migrate/20190619134726_add_submitted_to_pol_to_pafs_core_projects.rb
+++ db/migrate/20190619134726_add_submitted_to_pol_to_pafs_core_projects.rb
@@ -1,4 +1,4 @@
-class AddSubmittedToPolToPafsCoreProjects < ActiveRecord::Migration
+class AddSubmittedToPolToPafsCoreProjects < ActiveRecord::Migration[4.2]
   def change
     add_column :pafs_core_projects, :submitted_to_pol, :datetime
     add_index :pafs_core_projects, :submitted_to_pol
