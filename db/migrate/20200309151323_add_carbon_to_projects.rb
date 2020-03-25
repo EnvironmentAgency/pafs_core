de378ab01e879c0341f6ce0a3642788424f5b892
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-25 18:59:39 +0000

    [PM-587] Fix migration inheritence

--- db/migrate/20200309151323_add_carbon_to_projects.rb
+++ db/migrate/20200309151323_add_carbon_to_projects.rb
@@ -1,4 +1,4 @@
-class AddCarbonToProjects < ActiveRecord::Migration
+class AddCarbonToProjects < ActiveRecord::Migration[4.2]
   def change
     add_column :pafs_core_projects, :carbon_cost_build, :integer, limit: 8
     add_column :pafs_core_projects, :carbon_cost_operation, :integer, limit: 8
