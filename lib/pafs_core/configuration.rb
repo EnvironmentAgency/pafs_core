bb2a18f85c86976ee1ab837349126610b804ef1d
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-07-22 12:49:28 +0100

    Refactor

--- lib/pafs_core/configuration.rb
+++ lib/pafs_core/configuration.rb
@@ -12,25 +12,6 @@
 #   PafsCore.config.banner_feedback_uri
 #
 module PafsCore
-  # Enable the ability to configure the gem from its host app, rather than
-  # reading directly from env vars. Derived from
-  # https://robots.thoughtbot.com/mygem-configure-block
-  class << self
-    attr_writer :configuration
-
-    def configuration
-      @configuration ||= Configuration.new
-    end
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-
-  def self.start_airbrake
-    DefraRuby::Alert.start
-  end
-
   class Configuration
     include ActiveSupport::Configurable
 
